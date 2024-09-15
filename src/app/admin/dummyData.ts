1072bef9edb85d0e4a535db8e667de77d5449916
Angel Tapia <angeltapia1805@gmail.com>
2024-09-14 20:38:05 -0600

    updating graphics

---
+++ src/app/admin/dummyData.ts
@@ -0,0 +1,190 @@
+export type Pasillo = {
+  id: number;
+  detalles: {
+    area: string;
+    cantidad: string;
+  }[];
+  fecha: string | Date;
+};
+
+export function getRandomInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const dummyDataPasillo: Pasillo[] = [
+  {
+    id: 1,
+    detalles: [
+      {
+        area: "Botana",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+      {
+        area: "Frutas",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+    ],
+    fecha: "Jan 2023",
+  },
+  {
+    id: 2,
+    detalles: [
+      {
+        area: "Botana",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+      {
+        area: "Frutas",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+    ],
+    fecha: "Feb 2023",
+  },
+  {
+    id: 3,
+    detalles: [
+      {
+        area: "Botana",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+      {
+        area: "Frutas",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+    ],
+    fecha: "Mar 2023",
+  },
+  {
+    id: 4,
+    detalles: [
+      {
+        area: "Botana",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+      {
+        area: "Frutas",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+    ],
+    fecha: "Apr 2023",
+  },
+  {
+    id: 5,
+    detalles: [
+      {
+        area: "Botana",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+      {
+        area: "Frutas",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+    ],
+    fecha: "May 2023",
+  },
+  {
+    id: 6,
+    detalles: [
+      {
+        area: "Botana",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+      {
+        area: "Frutas",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+    ],
+    fecha: "Jun 2023",
+  },
+  {
+    id: 7,
+    detalles: [
+      {
+        area: "Botana",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+      {
+        area: "Frutas",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+    ],
+    fecha: "Jul 2023",
+  },
+  {
+    id: 8,
+    detalles: [
+      {
+        area: "Botana",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+      {
+        area: "Frutas",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+    ],
+    fecha: "Aug 2023",
+  },
+  {
+    id: 9,
+    detalles: [
+      {
+        area: "Botana",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+      {
+        area: "Frutas",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+    ],
+    fecha: "Sep 2023",
+  },
+  {
+    id: 10,
+    detalles: [
+      {
+        area: "Botana",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+      {
+        area: "Frutas",
+        cantidad: getRandomInt(1, 1000).toString(),
+      },
+    ],
+    fecha: "Oct 2023",
+  },
+];
+
+const transformedDataPasillo = dummyDataPasillo.reduce(
+  (acc, pasillo) => {
+    const fecha = pasillo.fecha;
+    if (!acc[fecha]) {
+      acc[fecha] = { fecha };
+    }
+
+    pasillo.detalles.forEach((detalle) => {
+      if (!acc[fecha][detalle.area]) {
+        acc[fecha][detalle.area] = 0;
+      }
+      acc[fecha][detalle.area] += parseInt(detalle.cantidad);
+    });
+
+    return acc;
+  },
+  {} as Record<string, any>,
+);
+
+export const finalDataPasillo = Object.values(transformedDataPasillo);
+
+//----
+
+export const sentimentData = [
+  { name: "Positive", value: getRandomInt(1, 100) },
+  { name: "Neutral", value: getRandomInt(1, 100) },
+  { name: "Negative", value: getRandomInt(1, 100) },
+];
+
+export const tiempoFila = [
+  { name: "Fila 1", value: getRandomInt(1, 30) },
+  { name: "Fila 2", value: getRandomInt(1, 30) },
+  { name: "Fila 3", value: getRandomInt(1, 30) },
+];
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,34 +5,14 @@ import { DataTable } from "./data-table";
 import { Button } from "r/components/ui/button";
 import { columns } from "./columns";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-
-type Pasillo = {
-  id: number;
-  nombre: string;
-  cantidad: string;
-  fecha: Date;
-};
-
-const dummyData: Pasillo[] = [
-  {
-    id: 1,
-    nombre: "Frutas",
-    cantidad: "10",
-    fecha: new Date("2023-01-01"),
-  },
-  {
-    id: 2,
-    nombre: "Botana",
-    cantidad: "20",
-    fecha: new Date("2023-02-01"),
-  },
-  {
-    id: 3,
-    nombre: "Bebidas",
-    cantidad: "30",
-    fecha: new Date("2023-03-01"),
-  },
-];
+import { v4 as uuidv4 } from "uuid";
+import { Card, CategoryBar, BarList, LineChart } from "@tremor/react";
+import {
+  finalDataPasillo,
+  getRandomInt,
+  sentimentData,
+  tiempoFila,
+} from "./dummyData";
 
 export default function AdminDashboard() {
   const fileInputRef = useRef<HTMLInputElement | null>(null);
@@ -47,7 +27,8 @@ export default function AdminDashboard() {
       try {
         const { data, error } = await supabase.storage
           .from(bucket)
-          .upload(file.name, file);
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument
+          .upload(uuidv4(), file);
         if (error) {
           console.error("Error during upload:", error);
         } else {
@@ -55,6 +36,10 @@ export default function AdminDashboard() {
         }
       } catch (error) {
         console.error("Error during upload:", error);
+      } finally {
+        if (fileInputRef.current) {
+          fileInputRef.current.value = "";
+        }
       }
     }
   };
@@ -81,9 +66,81 @@ export default function AdminDashboard() {
         />
       </div>
 
-      <h2 className="mb-6 text-3xl font-semibold text-gray-700">My Data</h2>
+      <h2 className="mb-6 text-3xl font-semibold text-gray-700">Data</h2>
+      <h3>Aisle Data</h3>
 
-      <DataTable columns={columns} data={dummyData} />
+      <div className="my-10">
+        <Card>
+          <h3 className="text-tremor-content-strong dark:text-dark-tremor-content-strong text-lg font-medium">
+            Cantidad personas por pasillo y fecha
+          </h3>
+          <LineChart
+            data={finalDataPasillo}
+            index="fecha"
+            categories={["Botana", "Frutas"]}
+            xAxisLabel="Fecha"
+            yAxisLabel="Cantidad"
+            yAxisWidth={65}
+          />
+        </Card>
+      </div>
+
+      <div className="my-10 flex">
+        <Card className="w-1/2">
+          <h3 className="text-tremor-title text-tremor-content-strong dark:text-dark-tremor-content-strong font-medium">
+            Análisis sentimiento clientes después de su compra
+          </h3>
+          <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content mt-4 flex items-center justify-between">
+            <span>Sentimiento</span>
+            <span>Cantidad Clientes</span>
+          </p>
+          <BarList data={sentimentData} sortOrder="none" />
+        </Card>
+
+        <Card className="w-1/2">
+          <h3 className="text-tremor-title text-tremor-content-strong dark:text-dark-tremor-content-strong font-medium">
+            Cantidad espera por fila
+          </h3>
+          <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content mt-4 flex items-center justify-between">
+            <span>Fila</span>
+            <span>Tiempo (min)</span>
+          </p>
+          <BarList data={tiempoFila} sortOrder="descending" />
+        </Card>
+      </div>
+      <div className="space-y-3">
+        <p className="text-center font-mono text-sm text-slate-500">
+          Porcentaje Stock por area
+        </p>
+        <div className="flex justify-center">
+          <Card className="max-w-sm">
+            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content mt-4 flex items-center justify-between">
+              <span>Productos limpieza</span>
+            </p>
+            <CategoryBar
+              values={[40, 30, 20, 10]}
+              colors={["rose", "orange", "yellow", "emerald"]}
+              markerValue={getRandomInt(70, 100)}
+            />
+            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content mt-4 flex items-center justify-between">
+              <span>Bebidas</span>
+            </p>
+            <CategoryBar
+              values={[40, 30, 20, 10]}
+              colors={["rose", "orange", "yellow", "emerald"]}
+              markerValue={getRandomInt(40, 90)}
+            />
+            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content mt-4 flex items-center justify-between">
+              <span>Botana</span>
+            </p>
+            <CategoryBar
+              values={[40, 30, 20, 10]}
+              colors={["rose", "orange", "yellow", "emerald"]}
+              markerValue={getRandomInt(60, 83)}
+            />
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
